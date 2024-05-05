af23553bc6808a828ae037c47eae8f5cdba449cd
Richard Campos <richardsjcb@gmail.com>
2024-05-05 13:18:38 -0300

    Memory max set 4096

--- App Main + DB + APIs + BI/ecosystem.config.js
+++ App Main + DB + APIs + BI/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "app-unlz", script: "./server.js", watch: true, // Se refresca automaticamente con los nuevos cambios
-        max_memory_resize: '1024', // Vigila constantemente que solo se use 1GB
+        max_memory_resize: '4096', // Vigila constantemente que solo se use 1GB
         exec_mode: "cluster", // Usa cantidad de nucleos, para setear aplicaciones independientes
         instances: 1,// Le decimos que use una sola instancia en el cluster.
         cron_restart: "59 23 * * *", // Lo que decimos es que a esa hora reinicie: 11:59:00 se reinicie
