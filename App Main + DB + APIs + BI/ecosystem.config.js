dea468d9d511937f5e57e623fecc263f9b81401b
Richard Campos <richardsjcb@gmail.com>
2024-06-03 16:58:40 -0300

    fix pm2

--- App Main + DB + APIs + BI/ecosystem.config.js
+++ App Main + DB + APIs + BI/ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     apps: [{
-        name: "app-unlz", script: "./server.js", watch: true, // Se refresca automaticamente con los nuevos cambios
-        max_memory_resize: '4096', // Vigila constantemente que solo se use 1GB
-        exec_mode: "cluster", // Usa cantidad de nucleos, para setear aplicaciones independientes
-        instances: 1,// Le decimos que use una sola instancia en el cluster.
+        name: "app-edesur",
+        script: "./server.js",
+        watch: false, // Se refresca automaticamente con los nuevos cambios
+        max_memory_restart: '4G', // Reiniciar si se excede 4GB de memoria
+        exec_mode: "cluster", // Utiliza la cantidad de núcleos para crear instancias independientes
+        instances: "max", // Utiliza la cantidad máxima de instancias según
         cron_restart: "59 23 * * *", // Lo que decimos es que a esa hora reinicie: 11:59:00 se reinicie
         env: {
             NODE_ENV: "development",
