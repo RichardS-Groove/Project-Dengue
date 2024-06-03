9b9510709c5b38032ce8fcf14e1036c2a3469889
Richard Campos <richardsjcb@gmail.com>
2024-06-03 17:11:51 -0300

    fix cluster 1

--- App Main + DB + APIs + BI/ecosystem.config.js
+++ App Main + DB + APIs + BI/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         watch: false, // Se refresca automaticamente con los nuevos cambios
         max_memory_restart: '4G', // Reiniciar si se excede 4GB de memoria
         exec_mode: "cluster", // Utiliza la cantidad de núcleos para crear instancias independientes
-        instances: "max", // Utiliza la cantidad máxima de instancias según
+        instances: 1, // Utiliza la cantidad máxima de instancias según
         cron_restart: "59 23 * * *", // Lo que decimos es que a esa hora reinicie: 11:59:00 se reinicie
         env: {
             NODE_ENV: "development",
